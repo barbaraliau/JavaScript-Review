7e76b3d4c5f7a25e49c83fa3b2cbe3c883d571d9
Barbara Liau <bj.liau@gmail.com>
2015-01-14 16:54:55 -0700

    Completed favoriteThings, user, methodCollection problems

--- objectReview.js
+++ objectReview.js
@@ -2,17 +2,35 @@
 
   //Your code here
 
+  var favoriteThings = {
+    band: 'Weezer',
+    food: 'sushi',
+    person: 'Ryan',
+    book: 'Daddy Long Legs',
+    movie: 'Tron',
+    holiday: 'Christmas'
+  };
+
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
   //Your code here
+
+favoriteThings.car = 'Volkswagen';
+favoriteThings.brand = 'J. Crew';
 
 //Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'. 
 
   //Your code here
 
+  favoriteThings.food = 'Lettuce';
+  favoriteThings.book = '50 Shades of Gray';
+
 //Now, alert your favorite person, then alert your favorite book.
 
   //Your code here
+
+  alert(favorite.person);
+  alert(favorite.book);
 
 
 
@@ -20,7 +38,7 @@
 
 
 
-var user = {a
+var user = {
     name: 'Tyler McGinnis', 
     email: null,
     pwHash: 'U+Ldlngx2BYQk',
@@ -33,14 +51,23 @@ var user = {a
 
   //Your code here
 
+for (var key in user) {
+  if(!user[key])
+    delete user[key];
+}
+
 //Once you get your truthy object, Change the remaining values in the object to be specific to you (name: 'your name', username: 'your username'), rather than my information.
 
   //Your code here
+
+  user.name = 'Barbara';
+  user.username = 'bjliau';
 
 //Now console.log your object and make sure it looks right.
 
   //Your code here
 
+console.log(user);
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
@@ -51,13 +78,26 @@ var user = {a
 
   //Your code here
 
+  var methodCollection = {};
+
 //Now add two methods (functions that are properties on objects) to your methodCollection object. One called 'alertHello' which alerts 'hello' and another method called logHello which logs 'hello' to the console. 
 
   //Your code here
 
+  methodCollection.alertHello = function() {
+    alert('hello');
+  }
+
+  methodCollection.logHello = function() {
+    console.log('hello');
+  }
+
 //Now call your alertHello and logHello methods. 
 
   //Your code here
+
+  methodCollection.alertHello();
+  methodCollection.logHello();
 
 
 
@@ -67,6 +107,32 @@ var user = {a
 
 //write a function called voweler that accepts a string, and returns an object with the keys being all the vowels in that string, and the values being how many times that particular vowel was in the string.
 //voweler("This is a test") --> {i: 2, a: 1, e: 1};
+
+var voweler = function(string) {
+  var vowelObj = {};
+  var vowelArray = string.split("");
+  for (var i = 0; i < vowelArray.length; i++) {
+    if (vowelArray[i] === 'a' || 'e'|| 'i' ||'o' ||'u') {
+
+      }
+
+
+     
+
+      }
+    }
+  }
+
+
+}
+
+
+
+
+
+
+
+
 
 
 
