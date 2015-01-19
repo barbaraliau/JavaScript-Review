1a1457b887a54c35b68408fc85c9d88c7f580c4a
Barbara Liau <bj.liau@gmail.com>
2015-01-18 21:15:01 -0700

    completed problems

--- closureReview.js
+++ closureReview.js
@@ -11,6 +11,9 @@ var callFriend = function(){
 
   //code here
 
+var calling = callFriend();
+calling('435-215-9248');
+
 
 
 /*
@@ -20,3 +23,40 @@ Write a function that accepts a function as it's only argument and returns a new
 Once completed, add a second arguments that allows the function to be executed N number of times. After the function has been called N number of times, console.log('STAHHP');
 
 */
+
+var alertFn = function() {
+  alert("Another function");
+};
+
+
+var outerFn = function(otherFn) {
+  var i = true;
+  return function() {
+    if (i) {
+      otherFn();
+      }
+    i = false;
+  };
+};
+
+var callFunction = outerFn(alertFn);
+callFunction();
+
+////
+
+var outerFn = function(otherFn, N) {
+  return function() {
+    if (N) {
+      otherFn();
+      N--;
+    }
+    else {
+      console.log('STAHHP');
+    }
+  }  
+};
+
+
+var callFunction = outerFn(alertFn, 3);
+callFunction();
+
